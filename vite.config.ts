636e878904b92e1db4240b3d1432704a9d72e446
digambarrajaram <87692004+digambarrajaram@users.noreply.github.com>
2026-08-08 15:47:16 +0530

    Updated vite.config.ts to use native-compatible directory resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const __dirname = import.meta?.dirname ?? new URL('.', import.meta.url).pathname;
+
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
